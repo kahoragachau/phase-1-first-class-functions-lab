02930807c9636787d009c78333909fc616574b0a
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-18 19:01:36 +0300

    - Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => { return drivers.slice(0,2)}
+const returnLastTwoDrivers = () => {return drivers.slice(2,)}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+const createFareMultiplier = (n)=> {return ()=>{return n**2}}
+
+const fareDoubler = (n)=>{ return  n + n}
+const fareTripler = (n)=> {return n * 3}
+const fareQuintupler = (n) => {return n*4}
+
+
+const selectDifferentDrivers = (drivers,option) => { return option(drivers)}
